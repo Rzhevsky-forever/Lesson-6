b77e88f9bfcd08996e9c53bb06a6217d2be03b43
egorbarisovi4 <egorbarisovi4@gmail.com>
2019-10-13 01:58:52 +0500

    first

--- Lesson-6.js
+++ Lesson-6.js
@@ -1,1 +1,21 @@
 'use strict';
+
+var positions = [
+    'Телепорт бытовой VZHIH-101',
+    'Отвертка ультразвуковая WHO-D',
+    'Ховерборд Mattel 2016',
+    'Нейтрализатор FLASH black edition',
+    'Меч световой FORCE (синий луч)'
+  ];
+  
+  var prices = [
+    10000,
+    4800,
+    9200,
+    2500,
+    57000
+  ];
+  
+  var hitName = positions[2], hitPrice = prices[2];
+
+  
